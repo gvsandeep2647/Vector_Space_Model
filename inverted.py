cf0a30ae10c8a83aa592f1200664fbc8c24452e0
Kushagra Agrawal <kushagra106@gmail.com>
2016-09-18 14:11:03 +0530

    Changes to inverted.py
    Tested idf scoring on two lists

--- inverted.py
+++ inverted.py
@@ -1,13 +1,26 @@
 from collections import Counter, defaultdict
+import numpy
+import math
 def create_index (data):
 	index = defaultdict(list)
 	for i, tokens in enumerate(data):
 		for token in tokens:
 			index[token].append(i)
+				
 	return index
 
-index = create_index([['a','b'],['a','c']])
-print index.keys()
-print index['a']
-print index['b']
-		
+N = [['a','b'],['a','c']] 
+index = create_index(N)
+size = len(N)
+'''print index['a']
+print index['b']'''
+print size
+#tf = numpy.empty(len(index), dtype=object)
+df = []
+print len(index['a'])
+for token in index:
+	df.append(len(index[token]))
+idf=[]
+for x in xrange(len(df)):
+	idf.append(math.log10(size/df[x]))
+print idf
