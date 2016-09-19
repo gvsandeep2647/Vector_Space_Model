28c4b91768e01d7e3125992a6b41ea1921b0b4c7
greetsandeep <greetsandeep@gmail.com>
2016-09-19 17:46:15 +0530

    Index!

--- TUAW-dataset/data/new_inverted.py
+++ TUAW-dataset/data/new_inverted.py
@@ -1,5 +1,10 @@
 from main import megaList
+import time
 from collections import defaultdict
+
+start_time = time.time()
+
+
 ultraTitle = []
 ultraBlogger = []
 ultraCategories = []
@@ -10,24 +15,21 @@ def make_unique(l):
 	l = list(l)
 	return l
 
-def index1(l,d,k):
-	temp = []
-	for i in l:
-		for j in range(0,len(megaList)):
-			if i in megaList[j][k]:
-				temp1    = [0,0,0]
-				temp1[0] = i
-				temp1[1] = megaList[j][k].count(i)
-				temp1[2] = j
-				temp.append(temp1)
 
-	for i in temp:
-		if i[0] in d:
-			pass
-			#d[i[0]] = d[i[0]].append([i[1],i[2]])
-		else:
-			d[i[0]] = [i[1],i[2]]
-	print d['isight']
+def index1(l,d,k):
+	megaTemp = []
+	for i in range(0,len(l)):
+		temp = [0,0]
+		temp1 = [0]*len(megaList)
+		temp[1] = temp1
+		for j in range(0,len(megaList)):
+			if l[i] in megaList[j][k]:
+				temp[0] = l[i]
+				temp[1][j] = megaList[j][k].count(l[i])
+				megaTemp.append(temp)
+	for i in megaTemp:
+		d[i[0]] = i[1]
+	return d
 
 for row in megaList:
 	for j in row[0]:
@@ -42,14 +44,18 @@ for row in megaList:
 ultraTitle = make_unique(ultraTitle)
 dictTitle = {}
 dictTitle = index1(ultraTitle,dictTitle,0)
-# for i in ultraTitle:
-# 	for j in range(0,len(megaList)):
-# 		if i in megaList[j][0]:
-# 			dictTitle[i] = j
 
-#for d in dictTitle:
-#	print d, dictTitle[d]
 ultraBlogger = make_unique(ultraBlogger)
+dictBlogger = {}
+dictBlogger = index1(ultraBlogger,dictBlogger,2)
+
 ultraCategories = make_unique(ultraCategories)
+dictCategories = {}
+dictCategories = index1(ultraCategories,dictCategories,3)
+
 ultraPost = make_unique(ultraPost)
+dictPost = {}
+dictPost = index1(ultraPost,dictPost,4)
+
+print "--- %s seconds ---" % (time.time() - start_time)
 
