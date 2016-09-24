0f0c7040e0716503d7ca6a4f99e539b9e99ea125
greetsandeep <greetsandeep@gmail.com>
2016-09-24 22:33:08 +0530

    GUI DONE

--- TUAW-dataset/data/GUI.py
+++ TUAW-dataset/data/GUI.py
@@ -101,8 +101,8 @@ def show_entry_fields():
 		
 def printResult(searchResult):
 	global resultsFrame
-	text = Text(resultsFrame)
-
+	global text
+	text.delete("1.0",END)
 	for i in searchResult:
 		text.insert(END,i+'\n')
 	text.pack(side=TOP) 
@@ -349,6 +349,9 @@ def finalquery(temp,l):
 	return answer
 resultsFrame = Frame(root)
 resultsFrame.pack(side=TOP)
+text = Text(resultsFrame)
+
+
 root.mainloop()
 
 
